22b51630eeedb73b366c536c664ed60196572bbf
David Pacheco <disrael.pr@gmail.com>
2017-10-31 13:16:29 -0600

    tercera versrón preguntando alumnos

--- clasede31.cpp
+++ clasede31.cpp
@@ -1,24 +1,29 @@
 //promedio de graduación
 #include <iostream>
 using namespace std;
-int s=0;
-int edad[16];
 int main()
 {
-for (int i=0; i<16; i++)
+int s=0;
+size_t ta=1;
+
+cout<<"¿Cúantos alumnos son?"<<endl;
+cin>>ta;
+
+int *edad=new int[ta];
+for (int i=0; i<ta; i++)
   {
     cout<<"¿qué edad tienes?"<<endl;
     cin>>edad[i];
   }
-for (int i=0; i<16; i++)
+for (int i=0; i<ta; i++)
   {
     s+=(edad[i]+3);
   }
-for (int i=0; i<16; i++)
+for (int i=0; i<ta; i++)
   {
     cout<<edad[i]<<" , ";
     cout<<endl;
   }
- cout<<"El promedio al que se graduará este gurpo es de "<<s/16<<" años"<<endl;
+ cout<<"El promedio al que se graduará este gurpo es de "<<s/ta<<" años"<<endl;
 return 0;
 }
